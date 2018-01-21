b72dc1ebe0132847ff3d9287d4f035c042cb0f66
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-01-21 15:29:37 -0500

    partner activity made

--- app/src/main/java/nyc/c4q/ramonaharrison/buggyapp/FifthActivity.java
+++ app/src/main/java/nyc/c4q/ramonaharrison/buggyapp/FifthActivity.java
@@ -2,12 +2,47 @@ package nyc.c4q.ramonaharrison.buggyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FifthActivity extends AppCompatActivity {
+    EditText wordInput;
+    Button runButton;
+    TextView resultDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fifth);
+
+        wordInput = (EditText) findViewById(R.id.input);
+        runButton = (Button) findViewById(R.id.run_button);
+        resultDisplay = (TextView) findViewById(R.id.repeated_word);
+
+        runButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                repeatWord();
+            }
+        });
+    }
+
+    /**
+     * You take a word and create a string the show
+     * the word repeated for the amount of letters in the original word.
+     *
+     * This if for the person debugging
+     */
+    public void repeatWord() {
+        if (!wordInput.getText().equals(null)){
+            String retpeatedWord = "";
+            String word = wordInput.getText().toString();
+            for (int i = 0; i < word.length(); i++) {
+                retpeatedWord = word;
+            }
+            resultDisplay.setText(retpeatedWord);
+        }
     }
 }
